7d11de3658362315b9c1896ae2385f3df52a681e
enleightond <enleightond@yahoo.com>
2016-03-04 17:16:22 -0800

    actual recursive palindrome

--- pal_recursion.js
+++ pal_recursion.js
@@ -1,13 +1,20 @@
 function pal(n) {
-    if (n !== "") {
+    if (typeof n !== "string") {
         n = n.toString();
     }
-	var palValue = n.split('').reverse().join('');
-	
-	if (palValue === n) {
-		return true;
-	}
-	else {
-	    return false;
-	}
+    if (n.length < 2) {
+        return true;
+    }
+    
+    if (n[0] === n[n.length -1]) {
+        if (n.length <= 3) {
+            return true;
+        }
+        var s = n.substr(1, n.length -2);
+        return pal(s);
+    }
+    else {
+        return false;
+    }
+    
 }
